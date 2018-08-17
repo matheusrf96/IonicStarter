8c6fbd3dddf1e5008519aeea86c543a09c5af8ab
Matheus <matheusrf96@gmail.com>
2018-08-16 22:14:36 -0300

    Página de login criada

---
+++ src/pages/login/login.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, AlertController } from 'ionic-angular';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+    this.login = {}
+  }
+
+  loginForm() {
+    console.log(this.login.email);
+    console.log(this.login.senha);
+
+    const alert = this.alertCtrl.create({
+      title: 'Alert',
+      subTitle: `User: ${this.login.email} <br />Senha: ${this.login.senha}`,
+      buttons: ['Ok']
+    });
+
+    alert.present();
+  }
+
+}
