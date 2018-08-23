e2fd2732b8fd5265e3135cdf8d40b38f31faed81
Matheus <matheusrf96@gmail.com>
2018-08-23 20:22:03 -0300

    Novo estilo para o botão do pânico e instruções interativas

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +8,17 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   public contagem;
 
-  constructor(public navCtrl: NavController,) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
     this.contagem = 0;
+  }
+
+  ionicAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Alerta',
+      subTitle: 'O alerta foi enviado com sucesso!',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 
   ativar() {
@@ -18,14 +27,18 @@ export class HomePage {
 
     if(this.contagem === 1){
       document.getElementById('botao-panico').style.backgroundColor = "#e9d700";
+      document.getElementById('texto-apoio').innerHTML = "Continue pressionando...";
     }
     else if(this.contagem === 2){
       document.getElementById('botao-panico').style.backgroundColor = "#f53d3d";
+      document.getElementById('texto-apoio').innerHTML = "Enviar alerta!";
     }
     else if(this.contagem === 3){
+      this.ionicAlert();
       this.contagem = 0;
+
       document.getElementById('botao-panico').style.backgroundColor = "#32db64";
-      alert("Seu alerta foi enviado");
+      document.getElementById('texto-apoio').innerHTML = "Toque o botão Ativar por três vezes.";
     }
   }
 
