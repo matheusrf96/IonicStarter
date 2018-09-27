0a59b8e8546be2597abcfcd34a328305c0937f92
Matheus <matheusrf96@gmail.com>
2018-09-27 20:24:16 -0300

    Recepção dos alertas do webservices

--- src/pages/alertas/alertas.ts
+++ src/pages/alertas/alertas.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the AlertasPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @IonicPage()
 @Component({
@@ -14,8 +9,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'alertas.html',
 })
 export class AlertasPage {
+  public id = 2;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public alertas: Array<string>;
+  private url: string = `http://localhost/tcc/api/listar-alertas-enviados.php?id=${this.id}`;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.http.get(this.url).map(res => res.json())
+      .subscribe(data => {
+        this.alertas = data;
+    }); 
   }
 
   ionViewDidLoad() {
--- src/pages/mensagem/mensagem.ts
+++ src/pages/mensagem/mensagem.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 /**
  * Generated class for the MensagemPage page.
@@ -14,8 +16,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mensagem.html',
 })
 export class MensagemPage {
+  public id = 2;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public alertas: Array<string>;
+  private url: string = `http://localhost/tcc/api/listar-alertas-recebidos.php?id=${this.id}`;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+    this.http.get(this.url).map(res => res.json())
+      .subscribe(data => {
+        this.alertas = data;
+    }); 
   }
 
   ionViewDidLoad() {
